553f2cc985f64311d1e35a75d69f0198debe41e4
WenRunxia <runxiaw@illinois.edu>
2019-04-29 11:15:11 -0500

    try 3

--- app/src/main/java/com/example/finalproject/Dish_Screen.java
+++ app/src/main/java/com/example/finalproject/Dish_Screen.java
@@ -1,11 +1,13 @@
 package com.example.finalproject;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public final class Dish_Screen extends AppCompatActivity {
@@ -18,14 +20,25 @@ public final class Dish_Screen extends AppCompatActivity {
             Intent newintent = new Intent(this, Photo_Catcher.class);
             startActivity(newintent);
         });
-        Intent intent = getIntent();
         final TextView json = findViewById(R.id.jsonDoc);
         json.setMovementMethod(new ScrollingMovementMethod());
         final Button show = findViewById(R.id.Show);
         show.setOnClickListener(v -> {
             json.setVisibility(View.VISIBLE);
             json.setText(Processor.resultText);
+            showInfo();
         });
-
+    }
+    private void showInfo() {
+        String result;
+        final TextView info = findViewById(R.id.dishInfo);
+        final ImageView dish = findViewById(R.id.dishImage);
+        Drawable toshow;
+        if (Processor.resultText.contains("Beef")) {
+            result = Processor.readFromfile("beef/beef.txt", this);
+            info.setText(result);
+            toshow = Processor.loadPhoto("beef/beef.jpeg", this);
+            dish.setImageDrawable(toshow);
+        }
     }
 }
--- app/src/main/java/com/example/finalproject/Processor.java
+++ app/src/main/java/com/example/finalproject/Processor.java
@@ -1,75 +1,24 @@
 package com.example.finalproject;
 
-import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.graphics.PorterDuff;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
-import android.support.v7.app.AppCompatActivity;
-import android.text.InputType;
+import android.content.res.AssetManager;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 import android.util.SparseIntArray;
 import android.view.Surface;
+import android.graphics.drawable.Drawable;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.Volley;
-import android.graphics.Point;
-import android.graphics.Rect;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
-import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
-import com.google.firebase.ml.vision.document.FirebaseVisionCloudDocumentRecognizerOptions;
-import com.google.firebase.ml.vision.document.FirebaseVisionDocumentText;
-import com.google.firebase.ml.vision.document.FirebaseVisionDocumentTextRecognizer;
-import com.google.firebase.ml.vision.text.FirebaseVisionCloudTextRecognizerOptions;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
-import com.google.firebase.ml.vision.text.RecognizedLanguage;
 
-import java.util.Arrays;
-import java.util.List;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-
-import com.example.finalproject.Photo_Catcher;
-import com.example.finalproject.Dish_Screen;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 import java.io.File;
 import java.io.IOException;
@@ -111,28 +60,48 @@ public class Processor {
                                     }
                                 });
     }
-    private void processTextBlock(FirebaseVisionText result) {
-        String resultText = result.getText();
-        for (FirebaseVisionText.TextBlock block: result.getTextBlocks()) {
-            String blockText = block.getText();
-            Float blockConfidence = block.getConfidence();
-            List<RecognizedLanguage> blockLanguages = block.getRecognizedLanguages();
-            Point[] blockCornerPoints = block.getCornerPoints();
-            Rect blockFrame = block.getBoundingBox();
-            for (FirebaseVisionText.Line line: block.getLines()) {
-                String lineText = line.getText();
-                Float lineConfidence = line.getConfidence();
-                List<RecognizedLanguage> lineLanguages = line.getRecognizedLanguages();
-                Point[] lineCornerPoints = line.getCornerPoints();
-                Rect lineFrame = line.getBoundingBox();
-                for (FirebaseVisionText.Element element: line.getElements()) {
-                    String elementText = element.getText();
-                    Float elementConfidence = element.getConfidence();
-                    List<RecognizedLanguage> elementLanguages = element.getRecognizedLanguages();
-                    Point[] elementCornerPoints = element.getCornerPoints();
-                    Rect elementFrame = element.getBoundingBox();
-                }
+    public static String readFromfile(String fileName, Context context) {
+        StringBuilder returnString = new StringBuilder();
+        InputStream fIn = null;
+        InputStreamReader isr = null;
+        BufferedReader input = null;
+        try {
+            fIn = context.getResources().getAssets()
+                    .open(fileName, Context.MODE_WORLD_READABLE);
+            isr = new InputStreamReader(fIn);
+            input = new BufferedReader(isr);
+            String line = "";
+            while ((line = input.readLine()) != null) {
+                returnString.append(line);
             }
+        } catch (Exception e) {
+            e.getMessage();
+        } finally {
+            try {
+                if (isr != null)
+                    isr.close();
+                if (fIn != null)
+                    fIn.close();
+                if (input != null)
+                    input.close();
+            } catch (Exception e2) {
+                e2.getMessage();
+            }
+        }
+        return returnString.toString();
+    }
+    public static Drawable loadPhoto(String path, Context context) {
+        try {
+            // get input stream
+            InputStream ims = context.getAssets().open(path);
+            // load image as Drawable
+            Drawable d = Drawable.createFromStream(ims, null);
+            // set image to ImageView
+            return d;
+        }
+        catch(IOException ex) {
+            Log.d(TAG, "You mess up");
+            return null;
         }
     }
 }
